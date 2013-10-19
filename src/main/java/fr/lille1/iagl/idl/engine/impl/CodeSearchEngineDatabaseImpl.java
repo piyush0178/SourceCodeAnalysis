4600f2782c182cac3423347934421e7004b4000e
Jules Ivanic <jules.ivanic@gmail.com>
2013-10-19 23:33:53 +0200

    gros avancement sur la method findMethodByParamater (ou qq chose du genre)

--- src/main/java/fr/lille1/iagl/idl/engine/impl/CodeSearchEngineDatabaseImpl.java
+++ src/main/java/fr/lille1/iagl/idl/engine/impl/CodeSearchEngineDatabaseImpl.java
@@ -3,6 +3,7 @@ package fr.lille1.iagl.idl.engine.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.namespace.QName;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
@@ -110,38 +111,94 @@ public class CodeSearchEngineDatabaseImpl implements CodeSearchEngine {
 
 	@Override
 	public List<Method> findMethodsTakingAsParameter(final String typeName) {
-		final List<Method> methods = new ArrayList<Method>();
 		try {
+			final String query = "declare variable $file as xs:string external;"
+					+ " declare variable $typeName as xs:string external;"
+					+ " let $functions := "
+					+ "		for $function in doc($file)//unit/unit/function[parameter_list/param/decl/type/name = $typeName]"
+					+ " 	return"
+					+ " 	<function>"
+					+ "			<specifier>{$function/type/specifier/text()}</specifier>"
+					+ "			<type_name>{$function/type/name/text()}</type_name>"
+					+ "			<method_name>{$function/name/text()}</method_name>"
+					+ " 		<parameter_list>"
+					+ "			{"
+					+ "				for $param in $function/parameter_list/param"
+					+ "				return"
+					+ "				<param>"
+					+ "					<type>{$param/decl/type/name}</type>"
+					+ "					<name>{$param/name}</name>"
+					+ "				</param>"
+					+ "			}"
+					+ "			</parameter_list>"
+					+ "		</function>"
+					+ " return" + "	<functionList>{$functions}</functionList>";
 
-			final String xPathQuery = "//unit/unit/function[parameter_list/param/decl/type/name = '"
-					+ typeName + "']";
-
-			final String query = "for $x in doc('" + filePath + "')"
-					+ xPathQuery
-					+ " return ($x/type/specifier, $x/type/name, $x/name)";
-
-			final XQPreparedExpression preparedExp = connection
+			final XQPreparedExpression preparedQuery = connection
 					.prepareExpression(query);
 
-			final XQResultSequence results = preparedExp.executeQuery();
+			preparedQuery.bindString(new QName("file"), filePath, null);
+			preparedQuery.bindString(new QName("typeName"), typeName, null);
 
-			// TODO JIV : delete
-			System.out.println(results.getSequenceAsString(null));
+			final XQResultSequence resultSequence = preparedQuery
+					.executeQuery();
 
-			while (results.next()) {
-				parseMethodsTakingAsParameterResults(results
-						.getSequenceAsStream());
-			}
+			System.out.println(resultSequence.getSequenceAsString(null));
 
+			return parseFunctionToMethod(resultSequence.getSequenceAsStream());
 		} catch (final XQException e) {
-			throw new RuntimeException("FAIL : findMethodsTakingAsParameter("
-					+ typeName + ") : " + e.getMessage(), e);
+			throw new RuntimeException(
+					"Probléme lors la requête  : findMethodsTakingAsParameter("
+							+ typeName + ") : " + e.getMessage(), e);
 		} catch (final XMLStreamException e) {
-			throw new RuntimeException("FAIL : findMethodsTakingAsParameter("
-					+ typeName + ") : " + e.getMessage(), e);
+			throw new RuntimeException(
+					"Probléme lors du parsing du XML : findMethodsTakingAsParameter("
+							+ typeName + ") : " + e.getMessage(), e);
 		}
-		return methods;
+	}
 
+	/**
+	 * @param xmlReader
+	 * @throws XMLStreamException
+	 */
+	private List<Method> parseFunctionToMethod(final XMLStreamReader xmlReader)
+			throws XMLStreamException {
+		final List<Method> methodList = new ArrayList<Method>();
+		Method method = null;
+		while (xmlReader.hasNext()) {
+			xmlReader.next();
+			final int eventType = xmlReader.getEventType();
+			if (eventType == XMLStreamReader.END_ELEMENT) {
+				switch (xmlReader.getLocalName()) {
+				case "function":
+					// toutes les infos de la fonction été lu et enregistrer
+					// dans l'object Method, on peut l'ajouter à la liste
+					// résultat.
+					methodList.add(method);
+				default:
+					break;
+				}
+			}
+			if (eventType == XMLStreamReader.START_ELEMENT) {
+				switch (xmlReader.getLocalName()) {
+				case "function":
+					method = new Method();
+					break;
+				case "specifier":
+					method.setDeclaringType(findType(xmlReader.getElementText()));
+					break;
+				case "type_name":
+					method.setDeclaringType(findType(xmlReader.getElementText()));
+					break;
+				case "method_name":
+					method.setName(xmlReader.getElementText());
+					break;
+				default:
+					break;
+				}
+			}
+		}
+		return methodList;
 	}
 
 	/**
@@ -223,11 +280,17 @@ public class CodeSearchEngineDatabaseImpl implements CodeSearchEngine {
 				"Méthode impossible à implémenter en utilisant SrcML");
 	}
 
+	/**
+	 * Execute la query Xquery passé en paramétre.
+	 * 
+	 * @param query
+	 * @return
+	 * @throws XQException
+	 */
 	private XQResultSequence execute(final String query) throws XQException {
 		XQPreparedExpression xqpe;
 		xqpe = connection.prepareExpression(query);
 		return xqpe.executeQuery();
-
 	}
 
 }
