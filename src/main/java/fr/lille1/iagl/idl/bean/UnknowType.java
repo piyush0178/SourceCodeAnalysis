e7c49333a49870270f5d25615a351e423ed635e3
Jules Ivanic <jules.ivanic@gmail.com>
2013-10-20 19:18:05 +0200

    ajout d'un nouveau bean

---
+++ src/main/java/fr/lille1/iagl/idl/bean/UnknowType.java
@@ -0,0 +1,29 @@
+package fr.lille1.iagl.idl.bean;
+
+/**
+ * UnknowType class. Represent a type used in the document but never declared.
+ */
+public class UnknowType extends Type {
+
+	private static final long serialVersionUID = -4197324567787520137L;
+
+	/**
+	 * empty constructor
+	 */
+	public UnknowType() {
+		// empty constructor
+	}
+
+	/**
+	 * @param name
+	 * @param fullyQualifiedPackageName
+	 * @param kind
+	 * @param declaration
+	 */
+	public UnknowType(final String name,
+			final String fullyQualifiedPackageName, final TypeKind kind,
+			final Location declaration) {
+		super(name, fullyQualifiedPackageName, kind, declaration);
+	}
+
+}
