371b001d129e8d0fb4bcf0b1907697ea2aaa913d
Jules Ivanic <jules.ivanic@gmail.com>
2013-10-20 12:53:59 +0200

    réorganisation des resources

--- src/main/java/fr/lille1/iagl/idl/constantes/Constantes.java
+++ src/main/java/fr/lille1/iagl/idl/constantes/Constantes.java
@@ -4,12 +4,12 @@ public class Constantes {
 
 	public static final String USER_DIR = System.getProperty("user.dir");
 	public static final String JAVA_XML = USER_DIR
-			+ "\\src\\main\\resources\\java.xml";
+			+ "\\src\\main\\resources\\xml\\java.xml";
 	public static final String COMMONS_XML = USER_DIR
-			+ "\\src\\main\\resources\\commons-collections.xml";
+			+ "\\src\\main\\resources\\xml\\commons-collections.xml";
 	public static final String JFACE_XML = USER_DIR
-			+ "\\src\\main\\resources\\jface.xml";
+			+ "\\src\\main\\resources\\xml\\jface.xml";
 	public static final String LUCENE_XML = USER_DIR
-			+ "\\src\\main\\resources\\lucene.xml";
+			+ "\\src\\main\\resources\\xml\\lucene.xml";
 
 }
